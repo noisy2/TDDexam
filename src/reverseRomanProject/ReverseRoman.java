fbc15fc062f6b26aff832251dad67bd04d3f2eef
hyun <hyun@hyunui-MacBook-Pro.local>
2018-12-13 10:31:00 +0900

    [Green] for III

--- src/reverseRomanProject/ReverseRoman.java
+++ src/reverseRomanProject/ReverseRoman.java
@@ -5,11 +5,26 @@ public class ReverseRoman {
 	public static int of(String inputRoman) {
 		// TODO Auto-generated method stub
 		int outInteger = 0;
+		if ( "III".equals(inputRoman) ) {
+			for(int i=0; i< inputRoman.length(); i++) {
+				if("I".equals(inputRoman.substring(i, i+1))) {
+					outInteger+=1;
+				}
+			}
+		}
 		if ( "II".equals(inputRoman) ) {
-			outInteger = 2;
+			for(int i=0; i< inputRoman.length(); i++) {
+				if("I".equals(inputRoman.substring(i, i+1))) {
+					outInteger+=1;
+				}
+			}
 		}
 		if ( "I".equals(inputRoman) ) {
-			outInteger = 1;
+			for(int i=0; i< inputRoman.length(); i++) {
+				if("I".equals(inputRoman.substring(i, i+1))) {
+					outInteger+=1;
+				}
+			}
 		}
 		
 		return outInteger;
