b99046c63d69389af7007a18c6a4246821bf6167
hyun <hyun@hyunui-MacBook-Pro.local>
2018-12-13 11:05:17 +0900

    [R] for V

--- src/reverseRomanProject/ReverseRoman.java
+++ src/reverseRomanProject/ReverseRoman.java
@@ -1,14 +1,20 @@
 package reverseRomanProject;
 
 public class ReverseRoman {
-
+	static int romanUnits[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	static String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	
 	public static int of(String inputRoman) {
 		// TODO Auto-generated method stub
 		int outInteger = 0;
-		
+		int index=0;
 		if( "V".equals(inputRoman) ) {
-			outInteger = 5;
-			return outInteger;
+			for(int i=0; i<romanLetters.length; i++) {
+				if(inputRoman.substring(index).startsWith(romanLetters[i])) {
+					outInteger += romanUnits[i];
+					index += romanLetters[i].length();
+				}
+			}
 		}
 		if( "IV".equals(inputRoman) ) {
 			outInteger = 4;
