a3003cb5503606c419e3d384b65990403641d751
hyun <hyun@hyunui-MacBook-Pro.local>
2018-12-13 10:19:42 +0900

    [Green] for II

--- src/reverseRomanProject/ReverseRoman.java
+++ src/reverseRomanProject/ReverseRoman.java
@@ -2,9 +2,17 @@ package reverseRomanProject;
 
 public class ReverseRoman {
 
-	public static int of(String string) {
+	public static int of(String inputRoman) {
 		// TODO Auto-generated method stub
-		return 1;
+		int outInteger = 0;
+		if ( "II".equals(inputRoman) ) {
+			outInteger = 2;
+		}
+		if ( "I".equals(inputRoman) ) {
+			outInteger = 1;
+		}
+		
+		return outInteger;
 	}
 
 }
