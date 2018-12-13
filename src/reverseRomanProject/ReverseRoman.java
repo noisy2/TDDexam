4908b74958107b712b0ccecde97e819e3551d569
hyun <hyun@hyunui-MacBook-Pro.local>
2018-12-13 10:52:18 +0900

    [Green] for IV

--- src/reverseRomanProject/ReverseRoman.java
+++ src/reverseRomanProject/ReverseRoman.java
@@ -5,6 +5,11 @@ public class ReverseRoman {
 	public static int of(String inputRoman) {
 		// TODO Auto-generated method stub
 		int outInteger = 0;
+		
+		if( "IV".equals(inputRoman) ) {
+			outInteger = 4;
+			return outInteger;
+		}
 		for(int i=0; i< inputRoman.length(); i++) {
 			if("I".equals(inputRoman.substring(i, i+1))) {
 				outInteger+=1;
